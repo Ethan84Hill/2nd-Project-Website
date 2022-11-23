cd64ad1a5b5cda79555f002d6a7b7bdab56c0a2d
Ethan <ethan84hill@gmail.com>
2022-11-23 16:04:35 -0500

    layout complete

---
+++ routes/standings.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User.model');
+const isLoggedIn = require('../middleware/isLoggedIn')
+const isLoggedOut = require('../middleware/isLoggedOut')
+const axios = require("axios");
+
+
+
+
+
+
+module.exports = router;
