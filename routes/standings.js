9eff15a2952d0d3499a1fe947c3f6f80eeb56d7b
Ethan <ethan84hill@gmail.com>
2022-12-02 10:00:28 -0500

    added new api

--- routes/standings.js
+++ routes/standings.js
@@ -5,23 +5,31 @@ const User = require('../models/User.model');
 const isLoggedIn = require('../middleware/isLoggedIn')
 const isLoggedOut = require('../middleware/isLoggedOut')
 const axios = require("axios");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const options = {
     method: 'GET',
-    url: 'https://current-ufc-rankings.p.rapidapi.com/',
-    headers: {
-      'X-RapidAPI-Key': '07430a7a21mshc4688a3bcb7b790p19a96bjsn18ff70964c39',
-      'X-RapidAPI-Host': 'current-ufc-rankings.p.rapidapi.com'
-    }
+    url: 'https://ufcrankingapi.fly.dev/',
+    // headers: {
+    //   'X-RapidAPI-Key': '07430a7a21mshc4688a3bcb7b790p19a96bjsn18ff70964c39',
+    //   'X-RapidAPI-Host': 'current-ufc-rankings.p.rapidapi.com'
+    // }
   };
   
   router.get('/standings', (req, res, next) => {
-    axios.request(options)
-    .then((response) => {
-      console.log(response.data);
-      res.render('standings.hbs', {foundStandings: response.data})
-    })
-    .catch(err => console.error(err))
+    // axios.request(options)
+    // .then((response) => {
+    //   console.log(response.data);
+    //   res.render('standings.hbs', {foundStandings: response.data})
+    // })
+    // .catch(err => console.error(err))
+    fetch('https://ufcrankingapi.fly.dev/')
+      .then(fetchRes => fetchRes.json())
+      .then(json => {
+        console.log(json);
+        res.render('standings.hbs', {foundStandings: json})
+      })
+      .catch(err => console.error(err))
   })
 
 
